b74c9621013beae0705fe23ee63b5713c4a3ecc1
sanjaypj <sanjaypjayan2000@gmail.com>
2019-12-01 15:48:21 -0500

    completed

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var $ = require('gulp-load-plugins')();
+var autoprefixer = require('autoprefixer');
+
+var sassPaths = [];
+
+function sass() {
+    return gulp.src('scss/app.scss')
+        .pipe($.sass({
+                includePaths: sassPaths,
+                outputStyle: 'compressed' // if css compressed **file size**
+            })
+            .on('error', $.sass.logError))
+        .pipe($.postcss([
+            autoprefixer({ browsers: ['last 2 versions', 'ie >= 9'] })
+        ]))
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.stream());
+};
+
+function serve() {
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("scss/*.scss", sass);
+    gulp.watch("*.html").on('change', browserSync.reload);
+}
+
+gulp.task('sass', sass);
+gulp.task('serve', gulp.series('sass', serve));
+gulp.task('default', gulp.series('sass', serve));
